603a44f4fbf7191f82bd52c0617ef3089643a159
[iiGamezzHN] <[davidarisz@gmail.com]>
2018-12-04 22:56:33 +0100

    Added changes

--- app/src/main/java/com/example/davidarisz/restaurant/CategoriesActivity.java
+++ app/src/main/java/com/example/davidarisz/restaurant/CategoriesActivity.java
@@ -3,7 +3,13 @@ package com.example.davidarisz.restaurant;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class CategoriesActivity extends AppCompatActivity {
+
+    public static void gotCategories(ArrayList categories) {
+        ;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/davidarisz/restaurant/CategoriesRequest.java
+++ app/src/main/java/com/example/davidarisz/restaurant/CategoriesRequest.java
@@ -1,10 +1,60 @@
 package com.example.davidarisz.restaurant;
 
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.util.ArrayList;
 
-public class CategoriesRequest {
+public class CategoriesRequest implements Response.Listener<JSONObject>, Response.ErrorListener {
+    Context context;
     public interface Callback {
         void gotCategories(ArrayList<String> categories);
         void gotCategoriesError(String message);
+    }
+
+    public CategoriesRequest(Context con) {
+        context = con;
+    }
+
+    public void getCategories(Callback activity) {
+        Callback callback = activity;
+        String url = "https://resto.mprog.nl/";
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, this, this);
+        queue.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        JSONArray values;
+        ArrayList arrayList = new ArrayList();
+
+        try {
+            values = response.getJSONArray("categories");
+
+            for (int i = 0; i < values.length(); i++) {
+                JSONObject categories = values.getJSONObject(i);
+                arrayList.add(categories.getString("categories"));
+            }
+
+            CategoriesActivity.gotCategories();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
